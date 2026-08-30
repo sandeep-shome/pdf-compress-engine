d6d8d32704d3980dd09388cef8cdc8c92730db93
sandeep <sandeepshome10@gmail.com>
2026-08-31 00:28:32 +0530

    feat: add static pages

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,14 @@
 import express, { type Express } from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 const app: Express = express();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/health', (req, res) => {
   res.json({
